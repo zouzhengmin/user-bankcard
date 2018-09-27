f79fd6bbfcc57e3629eaa01c5c909689869ba988
zhengmin <zouzhengmin@gmail.com>
2018-09-27 11:55:11 +0800

    create bank_card#edit#update#destroy

--- app/controllers/bank_cards_controller.rb
+++ app/controllers/bank_cards_controller.rb
@@ -1,7 +1,11 @@
 class BankCardsController < ApplicationController
 
+  before_action :find_bank_card, only: [:show, :edit, :update, :destroy]
   def index
     @bank_cards = BankCard.includes(:user)
+  end
+
+  def show
   end
 
   def new
@@ -9,8 +13,21 @@ class BankCardsController < ApplicationController
   end
 
   def create
-    bank_card = BankCard.new(bank_card_params)
-    bank_card.save
+    @bank_card = BankCard.new(bank_card_params)
+    @bank_card.save
+    redirect_to bank_cards_path
+  end
+
+  def edit
+  end
+
+  def update
+    @bank_card.update(bank_card_params)
+    redirect_to bank_cards_path
+  end
+
+  def destroy
+    bank_card.delete
     redirect_to bank_cards_path
   end
 
@@ -18,6 +35,10 @@ class BankCardsController < ApplicationController
 
   def bank_card_params
     params.require(:bank_card).permit(:user_id, :number, :bank_name, :status)
+  end
+
+  def find_bank_card
+    @bank_card = BankCard.find(params[:id])
   end
 
 
